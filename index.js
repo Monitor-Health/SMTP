11b3113d30909e574db6addb0e67264576e5d1d4
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2023-05-26 14:56:23 -0300

    Adicionando o CORS no index.js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const nodemailer = require('nodemailer')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
 
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
-    auth:{
+    auth: {
         user: 'thaisanobrega.com@gmail.com',
         pass: 'wircmzbszlpjowdu'
     }
 })
+app.use(cors());
+app.listen(3000, () => console.log("Server is running on port 3000"));
 
-transport.sendMail({
-    from: 'Thaisa <thaisanobrega.com@gmail.com>',
-    to: 'guilherme.scarabelli@gmail.com',
-    subject: 'Teste Monitr Health SMTP',
-    html: '<h1>OI</h1>',
-    text: 'EMAIL ENVIADO DE UMA API CRIDA POR MIMMMMMMMMMMM OLÁ GUIHERMEEEEEEEEEEEEEEEEEEEEEEEE'
+app.post("/sendmail", bodyParser.json(), (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    const body = req.body;
+    const from = body.from;
+    const name = body.name;
+    const subject = body.subject;
+    const text = body.text;
+    transport.sendMail({
+        from: `${from}`,
+        to: 'thaisanobrega.com@gmail.com',
+        subject: `${subject}`,
+        html: `<h1>${name}</h1><p>${text}</p><p>De: ${from}</p>`
+    })
+    .then(() => res.send("Email sent"))
+    .catch((err) => console.log(err))
+
 })
-.then(() => console.log('CERTOOOOOOOOOOOOOOOOOOOOOOOOOO') )
-.catch((err) => console.log("RUIMMMMMMMMMMMMMMMMMMMMMM:(", err))
+
